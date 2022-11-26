2332b805358443911db96ad0b6f0015e65abf11c
Blake Astley <astley92@hotmail.com>
2022-11-27 10:30:56 +1100

    use consistent module structure

--- lib/aocli/cli.rb
+++ lib/aocli/cli.rb
@@ -1,15 +1,17 @@
-module Aocli::Cli
-  module_function
+module Aocli
+  module Cli
+    module_function
 
-  def call
-    prompt = TTY::Prompt.new
-    run_for_current_day = prompt.yes?("Run for the current day?")
+    def call
+      prompt = TTY::Prompt.new
+      run_for_current_day = prompt.yes?("Run for the current day?")
 
-    raise NotImplementedError unless run_for_current_day
+      raise NotImplementedError unless run_for_current_day
 
-    {
-      date: Date.today,
-      output_destination: "."
-    }
+      {
+        date: Date.today,
+        output_destination: "."
+      }
+    end
   end
 end
