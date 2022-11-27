00d9faa3bc2bfe12ee04bfec5aac89088522f632
Blake Astley <astley92@hotmail.com>
2022-11-28 00:27:00 +1100

    Prompt user for year and day

--- lib/aocli/cli.rb
+++ lib/aocli/cli.rb
@@ -1,15 +1,17 @@
 module Aocli
   module Cli
+    YEARS = [2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015]
+    DAYS = (1..25).to_a
     module_function
 
     def call
       prompt = TTY::Prompt.new
-      run_for_current_day = prompt.yes?("Run for the current day?")
 
-      raise NotImplementedError unless run_for_current_day
+      year = prompt.select("What year", *YEARS)
+      day = prompt.select("What day", *DAYS)
 
       {
-        date: Date.today,
+        date: Date.new(year, 12, day),
         output_destination: "."
       }
     end
