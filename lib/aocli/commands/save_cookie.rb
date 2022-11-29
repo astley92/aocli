77fffdcdd1f7e7fb995fd478bf2d07c51363d69b
Blake Astley <astley92@hotmail.com>
2022-11-29 22:44:11 +1100

    Add message for user after save cookie

--- lib/aocli/commands/save_cookie.rb
+++ lib/aocli/commands/save_cookie.rb
@@ -4,6 +4,13 @@ module Aocli
       def self.run!(options)
         Aocli::FileUtils.touch_file(Aocli::Values::CONFIG_FILE_PATH)
         File.write(Aocli::Values::CONFIG_FILE_PATH, {cookie: options[:cookie]}.to_yaml)
+
+        puts <<~MSG
+          ### Cookie saved to #{Aocli::Values::CONFIG_FILE_PATH} ###
+
+          If you ever need to reset this cookie, you can use the save cookie command again
+          or simply edit that file.
+        MSG
       end
     end
   end
